ec6f03fde54cd1cf781551f1eeda79528c18009c
James Munnelly <james@munnelly.eu>
2015-05-05 16:49:39 +0100

    disable plexauth admin interface

--- plexauth/admin.py
+++ plexauth/admin.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import PlexUser
-# Register your models here.
+#from django.contrib import admin
+#from django.contrib.auth.admin import UserAdmin
+#from .models import PlexUser
+## Register your models here.
 
-class PlexUserAdmin(UserAdmin):
-    model = PlexUser
+#class PlexUserAdmin(UserAdmin):
+#    model = PlexUser#
 
-    fieldsets = UserAdmin.fieldsets
-    fieldsets[0][1]['fields'] += ('token', )
+#    fieldsets = UserAdmin.fieldsets
+#    fieldsets[0][1]['fields'] += ('token', )
 
-admin.site.register(PlexUser, PlexUserAdmin)
+#admin.site.register(PlexUser, PlexUserAdmin)
